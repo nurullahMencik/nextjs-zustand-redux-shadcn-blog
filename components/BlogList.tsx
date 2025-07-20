70bdbef7a3e7eecc256a79a0ac87d4b459d6a395
Nurullah Mencik <nurullahmencik42@gmail.com>
2025-07-20 14:54:56 +0300

    nextjs-zustand-redux-blog
    Next.js (app router) kullanarak SSR ve client-side fetch işlemleri içeren bir blog sitesi. React state yönetimini öğrenmek amacıyla Zustand ve Redux Toolkit ile geliştirilmiştir. Modern React hook’ları, API entegrasyonu ve ortam değişkenleri kullanımı pratiği yapılmıştır.
    
    Teknolojiler: Next.js 15, React, Zustand, Redux Toolkit, TypeScript ve Tailwind CSS 4.
    
    Amaç: React’te sunucu tarafı render (SSR), client tarafı veri çekme ve kapsamlı state management konularını öğrenmek ve uygulamaktır.

--- components/BlogList.tsx
+++ components/BlogList.tsx
@@ -1,26 +1,32 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import {Post} from "../types/pages/post"
+import { Post } from "../types/pages/post";
+
 const BlogList = () => {
   const [posts, setPosts] = useState<Post[]>([]);
 
   useEffect(() => {
-    const fetchProducts = async () => {
-      const res = await fetch("http://localhost:3000/api/blog");
+    const fetchPosts = async () => {
+      const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || ""; // "" olursa relative fetch olur
+      const res = await fetch(`${baseUrl}/api/blog`);
+      if (!res.ok) {
+        console.error("Failed to fetch posts");
+        return;
+      }
       const data = await res.json();
       setPosts(data);
     };
-    fetchProducts();
+    fetchPosts();
   }, []);
 
   return (
     <div>
-     <ul className='space-y-4'>
-        {posts && posts.map((post)=>(
-            <li key={post.id }>
-                <h2 className='text-xl'>{post.title}</h2>
-                <p>{post.content}</p>
-            </li>
+      <ul className="space-y-4">
+        {posts.map((post) => (
+          <li key={post.id}>
+            <h2 className="text-xl">{post.title}</h2>
+            <p>{post.content}</p>
+          </li>
         ))}
       </ul>
     </div>
